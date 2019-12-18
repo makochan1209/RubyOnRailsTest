d3799387235964a5e2dbaae71bcc124ed66610fe
Makoto Yamashita <makochan12.9@gmail.com>
2019-12-18 21:59:22 +0900

    これでよりわかりやすくなったかな

--- app/controllers/visitor_controller.rb
+++ app/controllers/visitor_controller.rb
@@ -1,9 +1,11 @@
 #コントローラは何も書かなくても自動でページを呼んでくれるようだ。
 #Route Globbingを採用
 class VisitorController < ApplicationController
+    #ここに企画一覧の出力など、特殊なページを書く。
   def show
     #@path = params[:path]
-    render params[:path]
+    #ここにはコントローラ名を載せる！(コントローラのメインディレクトリだと、コントローラの中から数えてくれるが、サブディレクトリだとviews/から考えてしまうので、コントローラ名/で書けば万事OKかなと)
+    render "visitor/" + params[:path]
   end
   #def about
   #end
