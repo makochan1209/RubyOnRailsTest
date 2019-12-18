365838ecca60383d0a530824831a345053406830
Makoto Yamashita <makochan12.9@gmail.com>
2019-12-18 19:58:22 +0900

    ルーティングを逐一しなくてよくなった！！！！！(Route Globbing)

--- app/controllers/visitor_controller.rb
+++ app/controllers/visitor_controller.rb
@@ -1,5 +1,10 @@
 #コントローラは何も書かなくても自動でページを呼んでくれるようだ。
+#Route Globbingを採用
 class VisitorController < ApplicationController
+  def show
+    #@path = params[:path]
+    render params[:path]
+  end
   #def about
   #end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,19 @@
 #routes.rbはちゃんと書かないといけない！(サーバー起動のままで更新できる)
+#Route Globbingを採用したので更新しなくてよい
 Rails.application.routes.draw do
   root to: 'visitor#index'
 
-  get 'visitor/about'
-  get 'visitor/event'
-  get 'visitor/access'
-  get 'visitor/guide'
-  get 'visitor/map'
-  get 'visitor/bus'
-  get 'visitor/note'
-  get 'visitor/timetable'
-  get 'visitor/yaguchi'
-  get 'visitor/feature'
+  get '*path', to: 'visitor#show'
+
+#  get 'visitor/about'
+#  get 'visitor/event'
+#  get 'visitor/access'
+#  get 'visitor/guide'
+#  get 'visitor/map'
+#  get 'visitor/bus'
+#  get 'visitor/note'
+#  get 'visitor/timetable'
+#  get 'visitor/yaguchi'
+#  get 'visitor/feature'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
