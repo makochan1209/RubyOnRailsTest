dde2e2b4ea2a4f2dd7f1a6fad1ac72abb2275d80
Makoto Yamashita <makochan12.9@gmail.com>
2019-12-19 22:00:32 +0900

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Eventslist.create(event_name: "企画名", group_name: "まこちゃん", event_desc: "イベント説明", event_appeal: "アピールポイント", building: 52, floor: 2, room: 201)
