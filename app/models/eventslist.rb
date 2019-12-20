711fc643776c6f7db1dcdfff6a236b0787c2f1ec
Makoto Yamashita <makochan12.9@gmail.com>
2019-12-20 12:45:04 +0900

    delete model

--- app/models/eventslist.rb
+++
@@ -1,2 +0,0 @@
-class Eventslist < ApplicationRecord
-end
--- db/seeds.rb
+++
@@ -1,8 +0,0 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-Eventslist.create(event_name: "企画名", group_name: "まこちゃん", event_desc: "イベント説明", event_appeal: "アピールポイント", building: 52, floor: 2, room: 201)
